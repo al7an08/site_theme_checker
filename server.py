bc9c59592e1959309cad4d168f395ecde5d6d99a
Evgenia Kositsyna <kositzina.eugenia@yandex.ru>
2023-09-30 17:52:56 +0300

    fix_ssl

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 ﻿from flask import Flask, jsonify, request
-
+from fake_useragent import UserAgent
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import time
@@ -12,40 +12,46 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import os
 os.environ['CURL_CA_BUNDLE'] = ''
+
+
 def parser(domain):
-    url = f'https://{domain}'
+    url = f'http://{domain}'
     
     session = requests.Session()
-    
-    
-    #time.sleep((20-5)*np.random.random()+5)
-    session.headers  = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    #'accept-encoding': 'gzip, deflate, br', # удалите эту строку
-    'accept-language': 'en-US,en;q=0.8',
-    'upgrade-insecure-requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 4.0.4; BNTV600 Build/IMM76L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.111 Safari/537.36'}
+    retry = Retry(connect=3, backoff_factor=1)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    ua = UserAgent()
+    header = {'User-Agent': str(ua.chrome)}
 
     #response = requests.get(url, verify=False)
-    response = session.get(url, verify=False,  proxies={"http": "http://111.233.225.166:1234"})
+    response = session.get(url, verify=False,  headers=header, timeout=30)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup((response.text), 'html.parser')
         text = soup.get_text()
-        return(text)
+        return text
     else:
-        return(f"Ошибка при запросе: {response.status_code}")
+        url = f'https://{domain}'
+        response = session.get(url, verify=False,  headers=header)
+        if response.status_code == 200:
+          
+            soup = BeautifulSoup(response.text, 'html.parser')
+            text = soup.get_text()
+            return text
+        else:
+            return(f"Ошибка при запросе: {response.status_code}")
 #www.gotennis.ru/
 @app.route('/check_domain', methods=['GET'])
 def check_domain():
-
-    
     domain = request.args.get('domain')
-
     if domain:
         result = parser(domain)
         return jsonify(result)
     else:
-        return jsonify({'error': 'Параметр domain отсутствует в запросе'}), 400
+        return jsonify({'error': 'Параметр domain отсутствует в запросе'})
 
 if __name__ == '__main__':
     app.config['JSON_AS_ASCII'] = False
